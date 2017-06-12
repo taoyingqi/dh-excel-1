1d5be824f2dbab8e9727d0acafaf40f3a2809604
taoyingqi <taoyingqi2013@sina.com>
2017-06-12 15:09:32 +0800

    First check

--- src/main/java/dh/data/App.java
+++ src/main/java/dh/data/App.java
@@ -1,12 +1,27 @@
 package dh.data;
 
+import dh.data.dao.MidDao;
+import dh.data.dao.UltimateDao;
+import dh.data.service.MidService;
+import dh.data.service.UltimateService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
 public class App {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    private static final Logger LOG = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args ) throws IOException {
+        LOG.info("[处理中间表]");
+        MidDao.clear();
+        MidService.calc();
+        LOG.info("[处理最终表]");
+        UltimateDao.clear();
+        UltimateService.calc();
     }
 }
